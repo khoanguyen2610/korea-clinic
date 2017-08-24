80637ac2deda320440cbc75e375783cfb29ca6dc
Khoa Nguyen Anh <k_nguyen@visionet.local>
2017-08-24 10:20:19 +0700

    merge

--- fuel/app/logs/2017/08/24.php
+++
@@ -1,5 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2017-08-24 12:14:11 --> Fuel\Core\Fuel::init - The configured locale LC_ALL.utf8 is not installed on your system.
-WARNING - 2017-08-24 12:14:47 --> Fuel\Core\Fuel::init - The configured locale LC_ALL.utf8 is not installed on your system.
-WARNING - 2017-08-24 12:17:29 --> Fuel\Core\Fuel::init - The configured locale LC_ALL.utf8 is not installed on your system.
