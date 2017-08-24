a0be30c6aa4c96a3faadb1ad2a0984e40eefa0ae
Khoa Nguyen Anh <k_nguyen@visionet.local>
2017-08-24 10:18:14 +0700

    add ignore

--- fuel/app/logs/2017/08/24.php
+++ fuel/app/logs/2017/08/24.php
@@ -2,3 +2,4 @@
 
 WARNING - 2017-08-24 12:14:11 --> Fuel\Core\Fuel::init - The configured locale LC_ALL.utf8 is not installed on your system.
 WARNING - 2017-08-24 12:14:47 --> Fuel\Core\Fuel::init - The configured locale LC_ALL.utf8 is not installed on your system.
+WARNING - 2017-08-24 12:17:29 --> Fuel\Core\Fuel::init - The configured locale LC_ALL.utf8 is not installed on your system.
