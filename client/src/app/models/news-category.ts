acf18be2123358ff06504fb874d0f7eaa1707053
DarKnight <ta.pnmluan@gmail.com>
2017-08-27 10:29:10 -0700

    Add service-category

---
+++ client/src/app/models/news-category.ts
@@ -0,0 +1,9 @@
+export class ServiceNews {
+	public id: number;
+	public lang_code: string;
+	public title: string;
+	public description: string;
+	public parent: number;
+	public level: number;
+	public language_name: string;
+}
