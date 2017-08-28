78d11480d8f44118480afd77fded896ed0591e05
Luan <l_phan@vision-net.co.jp>
2017-08-28 18:41:29 +0700

    Add common admin module

--- client/src/app/modules/admin/components/admin.module.ts
+++ client/src/app/modules/admin/components/admin.module.ts
@@ -3,12 +3,12 @@ import { Routing } from './admin.routing';
 import { CoreModule } from '../../../shared/modules/core.module';
 
 
-import { DashboardComponent, AppointmentComponent } from './';
+import { DashboardComponent, AppointmentComponent, AdminComponent } from './';
 import { MainNavComponent, HeaderComponent, FooterComponent, BreadcrumbComponent } from './general';
 
 
 @NgModule({
-    declarations: [DashboardComponent, AppointmentComponent, MainNavComponent, HeaderComponent, FooterComponent, BreadcrumbComponent],
+	declarations: [AdminComponent, DashboardComponent, AppointmentComponent, MainNavComponent, HeaderComponent, FooterComponent, BreadcrumbComponent],
     imports: [ CoreModule, Routing ]
 })
 export class AdminModule {}
