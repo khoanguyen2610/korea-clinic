4762ded8f0535d240b242639d7803e8e7859b10c
Luan <l_phan@vision-net.co.jp>
2017-09-12 12:33:48 +0700

    Fix bugs service-news list

--- client/src/app/modules/public/components/news/list/news-list.component.ts
+++ client/src/app/modules/public/components/news/list/news-list.component.ts
@@ -46,14 +46,6 @@ export class NewsListComponent implements OnInit {
 			(param: any) => this._params = param
 		);
 
-		this.subscriptionEvents = this._Router.events.subscribe((val) => {
-			let routing = this._Router.url;
-			if (this.curRouting != routing) {
-				this.curRouting = routing;
-				this.loadPage();
-			}
-		});
-
 		this.language_code = String(_LocalStorageService.get('language_code'));
 		if(this.language_code == 'en'){
 			this.controller = 'news';
@@ -62,6 +54,7 @@ export class NewsListComponent implements OnInit {
 	}
 
 	ngOnInit() {
+		this.loadPage();
 	}
 
 	loadPage(){
@@ -128,6 +121,5 @@ export class NewsListComponent implements OnInit {
 
 	ngOnDestroy() {
 		this.subscription.unsubscribe();
-		this.subscriptionEvents.unsubscribe();
 	}
 }
