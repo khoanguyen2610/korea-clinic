4dbf4e756eb983edbc390575799bcd7dbca54f2c
Khoa Nguyen Anh <k_nguyen@visionet.local>
2017-09-12 15:28:13 +0700

    merge

--- client/src/app/pipes/niceurl.pipe.ts
+++ client/src/app/pipes/niceurl.pipe.ts
@@ -17,6 +17,18 @@ export class NiceUrl implements PipeTransform {
   	}
 
 	urlify(a) {
-		return a.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-+|-+$/g, "-").replace(/^-+|-+$/g, '')
+		var newStr = a.toLowerCase().trim();
+        newStr = newStr.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+		console.log(newStr);
+        newStr = newStr.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+        newStr = newStr.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
+        newStr = newStr.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+        newStr = newStr.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+        newStr = newStr.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+        newStr = newStr.replace(/(đ)/g, 'd');
+        newStr = newStr.replace(/[^a-z0-9-\s]/g, '');
+        newStr = newStr.replace(/([\s]+)/g, '-');
+        newStr = newStr.replace(/-+/g, '-');
+		return newStr;
 	};
 }
