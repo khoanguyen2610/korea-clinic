adc91c10d48c8e2170fe075ec10e995fbe516ae2
Khoa Nguyen Anh <k_nguyen@visionet.local>
2017-09-13 18:43:33 +0700

    merge

--- client/src/app/modules/public/components/general/header-mobile/header-mobile.component.ts
+++ client/src/app/modules/public/components/general/header-mobile/header-mobile.component.ts
@@ -3,9 +3,11 @@ import { Location } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 import { URLSearchParams } from '@angular/http';
+import { LocalStorageService } from 'angular-2-local-storage';
 
+import { Configuration } from '../../../../../shared';
 
-// declare let $: any;
+declare let window: any;
 // declare let moment: any;
 
 @Component({
@@ -14,14 +16,21 @@ import { URLSearchParams } from '@angular/http';
 })
 
 export class HeaderMobileComponent implements OnInit {
-
+	default_language_code: string;
 	constructor(
-
+		private _LocalStorageService: LocalStorageService,
+		private _Configuration: Configuration
 	) {
-
+		this.default_language_code = String(this._LocalStorageService.get('language_code'));
 	}
 
 	ngOnInit() {
+	}
+
+	onChangeLanguageCode(language_code: string){
+		this._LocalStorageService.set('language_code', language_code);
+		window.location.reload();
+		return false;
 	}
 
 
